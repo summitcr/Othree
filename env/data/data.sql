7fdf5307731e58812d551d2f81e80de60b313a92
gustavo <gustavo@costaricasoftwareservices.com>
2021-01-19 00:27:11 -0600

    Second commit
    Adding some ignore files and removing unused env. folder

--- env/data/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO public."Custumer"(
-	 name, lastname, birth, email, phonenumber)
-	VALUES ('Alberto','Alvarado',NOW(), 'betoalvarado', '878787');
-
-INSERT INTO public."Custumer"(
-	 name, lastname, birth, email, phonenumber)
-	VALUES ('Lucía','Alvarado',NOW(), 'lalvarado', '88997744');
-	
